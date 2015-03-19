d0ce47c48c8ed62f95b31a9428a9ca84bde64049
Yan, Andrew N. <ayan@usgs.gov>
2015-03-19 16:13:10 -0500

    started some more stuff

--- pysgrid/sgrid.py
+++ pysgrid/sgrid.py
@@ -3,3 +3,19 @@ Created on Mar 19, 2015
 
 @author: ayan
 '''
+
+
+class SGrid(object):
+    
+    def __init__(self, nodes=None, faces=None, edges=None,
+                 node_padding=None, face_padding=None,
+                 edge_padding=None, grid_topology=None):
+        self.nodes = nodes
+        self.faces = faces
+        self.edges = edges
+        self.node_padding = node_padding
+        self.face_padding = face_padding
+        self.edge_padding = edge_padding
+        self.grid_topology = grid_topology
+        
+    
