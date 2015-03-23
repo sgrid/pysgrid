782dbf3f24701f08f28f88fdc82284ae27b9949f
Yan, Andrew N <ayan@usgs.gov>
2015-03-23 11:54:55 -0500

    added topology variable setter and property

--- pysgrid/sgrid.py
+++ pysgrid/sgrid.py
@@ -11,26 +11,33 @@ class SGrid(object):
     def __init__(self, nodes=None, faces=None, edges=None,
                  node_padding=None, face_padding=None,
                  edge_padding=None, grid_topology_vars=None):
-        self.nodes = nodes
-        self.faces = faces
-        self.edges = edges
-        self.node_padding = node_padding
-        self.face_padding = face_padding
-        self.edge_padding = edge_padding
-        self.grid_topology_vars = grid_topology_vars
+        self._nodes = nodes
+        self._faces = faces
+        self._edges = edges
+        self._node_padding = node_padding
+        self._face_padding = face_padding
+        self._edge_padding = edge_padding
+        self._grid_topology_vars = grid_topology_vars
         
     @classmethod
     def from_nc_file(cls, nc_url, grid_topology_vars=None, load_data=False):
         grid = cls()
-        sgrid_obj = load_grid_from_nc_file(nc_url, grid, 
-                                           grid_topology_vars, load_data
-                                           )
-        return sgrid_obj
+        load_grid_from_nc_file(nc_url, grid, 
+                               grid_topology_vars, load_data
+                               )
+        return grid
     
     @classmethod
     def from_nc_dataset(cls, nc_dataset, grid_topology_vars=None, load_data=False):
         grid = cls()
-        sgrid_obj = load_grid_from_nc_dataset(nc_dataset, grid, 
-                                              grid_topology_vars, load_data
-                                              )
-        return sgrid_obj
+        load_grid_from_nc_dataset(nc_dataset, grid, 
+                                  grid_topology_vars, load_data
+                                  )
+        return grid
+    
+    @property
+    def grid_topology_vars(self):
+        return self._grid_topology_vars
+    @grid_topology_vars.setter
+    def grid_topology_vars(self, grid_topo_vars):
+        self._grid_topology_vars = grid_topo_vars  
