457dcce3bf0fbc5fdfe834962eb204def0fcaad4
Yan, Andrew N <ayan@usgs.gov>
2015-03-25 09:58:31 -0500

    * added definition of slices based on padding * added exception for failure to find padding * added support to find grid cell center variables

---
+++ pysgrid/lookup.py
@@ -0,0 +1,14 @@
+'''
+Created on Mar 24, 2015
+
+@author: ayan
+'''
+
+
+LAT_GRID_CELL_CENTER_LONG_NAME = ['latitude of RHO-points',  #ROMS
+                                  'grid cell centers, latitude-coordinate',  #deltares
+                                  ] 
+
+LON_GRID_CELL_CENTER_LONG_NAME = ['longitude of RHO-points',  #ROMS
+                                  'grid cell centers, longitude-coordinate',  #deltares 
+                                  ]
