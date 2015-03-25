c3fab735df56bb14580745b9992cdd54216f0235
Yan, Andrew N. <ayan@usgs.gov>
2015-03-25 09:58:31 -0500

    * added definition of slices based on padding * added exception for failure to find padding * added support to find grid cell center variables

--- pysgrid/utils.py
+++ pysgrid/utils.py
@@ -5,11 +5,28 @@ Created on Mar 23, 2015
 '''
 import re
 from collections import namedtuple
+from custom_exceptions import CannotFindPadding
+
+'''
+    int grid;
+      :long_name = "";
+      :cf_role = "grid_topology";
+      :topology_dimension = 2; // int
+      :node_dimensions = "xi_psi eta_psi";
+      :face_dimensions = "xi_rho: xi_psi (padding: both) eta_rho: eta_psi (padding: both)";
+      :edge1_dimensions = "xi_u: xi_psi eta_u: eta_psi (padding: both)";
+      :edge2_dimensions = "xi_v: xi_psi (padding: both) eta_v: eta_psi";
+      :node_coordinates = "lon_psi lat_psi";
+      :face_coordinates = "lon_rho lat_rho";
+      :edge1_coordinates = "lon_u lat_u";
+      :edge2_coordinates = "lon_v lat_v";
+      :vertical_dimensions = "s_rho: s_w (padding: none)";
+'''
 
 
 GridPadding = namedtuple('GridPadding', ['mesh_topology_var',  # the variable containing the padding information
-                                         'dim_name',  # the topology attribute
-                                         'dim_var',  # node dimension within the topology attribute
+                                         'face_dim',  # the topology attribute
+                                         'node_dim',  # node dimension within the topology attribute
                                          'padding'  # padding type for the node dimension
                                          ]
                          )
@@ -21,6 +38,12 @@ class ParsePadding(object):
     variables with a cf_role of 'grid_topology'.
     
     """
+    padding_slices = {'both': (1, -1),
+                      'none': (None, None),
+                      'low': (1, None),
+                      'high': (None, 1)
+                      }
+    
     def __init__(self, mesh_topology_var=None):
         self.mesh_topology_var = mesh_topology_var
 
@@ -47,14 +70,14 @@ class ParsePadding(object):
         padding_matches = p.findall(padding_str)
         padding_type_list = []
         for padding_match in padding_matches:
-            raw_dim_name, raw_dim_var, raw_padding_var = padding_match
-            dim_name = raw_dim_name.split(':')[0]
-            dim_var = raw_dim_var
+            raw_face_dim, raw_node_dim, raw_padding_var = padding_match
+            face_dim = raw_face_dim.split(':')[0]
+            node_dim = raw_node_dim
             cleaned_padding_var = re.sub('[\(\)]', '', raw_padding_var)  # remove parentheses
             padding_type = cleaned_padding_var.split(':')[1].strip()  # get the padding value and remove spaces
             grid_padding = GridPadding(mesh_topology_var=self.mesh_topology_var,
-                                       dim_name=dim_name,
-                                       dim_var=dim_var,
+                                       face_dim=face_dim,
+                                       node_dim=node_dim,
                                        padding=padding_type
                                        )
             padding_type_list.append(grid_padding)
@@ -62,6 +85,20 @@ class ParsePadding(object):
             final_padding_types = padding_type_list
         else:
             final_padding_types = None
+            raise CannotFindPadding
         return final_padding_types
-        
+    
+    def define_recommended_slices(self, padding_str):
+        if padding_str is not None:
+            padding_data = self.parse_padding(padding_str)
+            rec_slices = [] 
+            for padding_datum in padding_data:
+                face_dim = '{0}_padding_slice'.format(padding_datum.face_dim)
+                padding_type = padding_datum.padding
+                padding_slices = self.padding_slices[padding_type]
+                rec_padding_slice = (face_dim, padding_slices)
+                rec_slices.append(rec_padding_slice)
+            return rec_slices
+        else:
+            raise CannotFindPadding
         
