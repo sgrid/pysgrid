b662000e5cf3607a9f113d26005655c2ff81d87e
Yan, Andrew N <ayan@usgs.gov>
2015-04-02 09:27:16 -0500

    added dimension mismatch exception

--- pysgrid/custom_exceptions.py
+++ pysgrid/custom_exceptions.py
@@ -24,3 +24,17 @@ class SGridNonCompliant(Exception):
     def __str__(self):
         error_message = self.base_message.format(self.dataset_obj)
         return error_message
+    
+    
+class DimensionMismatch(Exception):
+    
+    def __init__(self, arr_1_shape, arr_2_shape):
+        self.arr_1_shape = arr_1_shape
+        self.arr_2_shape = arr_2_shape
+        
+    def __str__(self):
+        error_message = ('The is a dimension mismatch between arrays with shape {0} and {1}. '
+                         'Arrays must have the same same to use this function.')
+        filled_message = error_message.format(self.arr_1_shape, self.arr_2_shape)
+        return filled_message
+        
