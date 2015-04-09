6ed1a49e919bb85c02194e5f3552bf980d17de5d
Yan, Andrew N <ayan@usgs.gov>
2015-04-09 09:13:59 -0500

    improved support for optional attributes in the SGRID grid variable

--- pysgrid/tests/test_sgrid.py
+++ pysgrid/tests/test_sgrid.py
@@ -29,7 +29,7 @@ class TestSGridCreate(unittest.TestCase):
         self.assertIsInstance(sg_obj, SGrid)
         
 
-class TestSGridWithEdgesAttributes(unittest.TestCase):
+class TestSGridWithOptionalAttributes(unittest.TestCase):
     
     def setUp(self):
         self.sgrid_test_file = os.path.join(TEST_FILES, 'test_sgrid_roms_like.nc')
@@ -68,7 +68,6 @@ class TestSGridWithEdgesAttributes(unittest.TestCase):
                          ]
         self.assertEqual(dataset_vars, expected_vars)
 
-        
     def test_variable_slicing(self):
         u_slices = self.sg_obj.u_slice
         v_slices = self.sg_obj.v_slice
@@ -76,6 +75,20 @@ class TestSGridWithEdgesAttributes(unittest.TestCase):
         v_expected = (np.s_[:], np.s_[:], np.s_[:], np.s_[1:-1])
         self.assertEqual(u_slices, u_expected)
         self.assertEqual(v_slices, v_expected)
+        
+    def test_optional_grid_attrs(self):
+        face_coordinates = self.sg_obj.face_coordinates
+        node_coordinates = self.sg_obj.node_coordinates
+        edge_1_coordinates = self.sg_obj.edge_1_coordinates
+        edge_2_coordinates = self.sg_obj.edge_2_coordinates
+        fc_expected = ('lon_center', 'lat_center')
+        nc_expected = ('lon_node', 'lat_node')
+        e1c_expected = ('lon_u', 'lat_u')
+        e2c_expected = ('lon_v', 'lat_v')
+        self.assertEqual(face_coordinates, fc_expected)
+        self.assertEqual(node_coordinates, nc_expected)
+        self.assertEqual(edge_1_coordinates, e1c_expected)
+        self.assertEqual(edge_2_coordinates, e2c_expected)
         
 
 class TestSGridWithoutEdgesAttributes(unittest.TestCase):
@@ -97,3 +110,15 @@ class TestSGridWithoutEdgesAttributes(unittest.TestCase):
         v_expected = (np.s_[:], np.s_[:], np.s_[:], np.s_[1:])
         self.assertEqual(u_slices, u_expected)
         self.assertEqual(v_slices, v_expected)
+        
+    def test_grid_optional_attrs(self):
+        face_coordinates = self.sg_obj.face_coordinates
+        node_coordinates = self.sg_obj.node_coordinates
+        edge_1_coordinates = self.sg_obj.edge_1_coordinates
+        edge_2_coordinates = self.sg_obj.edge_2_coordinates
+        fc_expected = ('XZ', 'YZ')
+        nc_expected = ('XCOR', 'YCOR')
+        self.assertEqual(face_coordinates, fc_expected)
+        self.assertEqual(node_coordinates, nc_expected)
+        self.assertIsNone(edge_1_coordinates)
+        self.assertIsNone(edge_2_coordinates)
