6e5e9a704dda8941d246de921b09002852989e11
Yan, Andrew N <ayan@usgs.gov>
2015-04-13 11:23:41 -0500

    used brackets to enclose long if statement

--- pysgrid/utils.py
+++ pysgrid/utils.py
@@ -99,14 +99,22 @@ def determine_variable_slicing(sgrid_obj, nc_dataset, variable, method='center')
     if method == 'center':
         for var_dim in var_dims:
             try:
-                if (sgrid_obj.edge_1_padding is None) and (sgrid_obj.edge_2_padding is None) and (variable in grid_variables):
+                if [(sgrid_obj.edge_1_padding is None) and 
+                    (sgrid_obj.edge_2_padding is None) and 
+                    (variable in grid_variables)
+                    ]:
                     # define padding for WRF or Deltares datasets
-                    padding_info = next((info for info in padding_summary if info[1] == var_dim))  # search through padding to find the variables dimension
-                elif (sgrid_obj.edge_1_padding is None) and (sgrid_obj.edge_2_padding is None) and (variable in grid_variables):
+                    # search through padding to find the variables dimension
+                    padding_info = next((info for info in padding_summary if info[1] == var_dim))
+                elif [(sgrid_obj.edge_1_padding is None) and 
+                      (sgrid_obj.edge_2_padding is None) and 
+                      (variable in grid_variables)
+                      ]:
                     padding_info = next((info for info in padding_summary if info[0] == var_dim))
                 else:
                     # failing that, try ROMS
-                    padding_info = next((info for info in padding_summary if info[0] == var_dim))  # search through padding to find the variables dimension
+                    # search through padding to find the variables dimension
+                    padding_info = next((info for info in padding_summary if info[0] == var_dim))
                 padding_val = padding_info[-1]
                 slice_datum = sgrid_obj.padding_slices[padding_val]
                 lower_slice, upper_slice = slice_datum
