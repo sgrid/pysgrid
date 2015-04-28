08decfdb57dc5fc2c5ca7eee2662e0ac47bd05fc
Yan, Andrew N <ayan@usgs.gov>
2015-04-28 08:03:33 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ setup(
     version='0.0.2',
     author='Andrew Yan',
     author_email='ayan@usgs.gov',
+    url='https://github.com/sgrid/pysgrid',
     packages=['pysgrid', 'pysgrid.tests'],
     license='BSD',
     description='Python package for working with staggered gridded data',
     long_description=open('README.md').read(),
+    install_requires=['netCDF4 == 1.1.7.1',
+                      'numpy == 1.9.2'
+                      ],
 )
