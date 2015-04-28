d977e570a98ba096bb37bbab33726b2aa3729757
Yan, Andrew N <ayan@usgs.gov>
2015-04-28 08:31:51 -0500

    updated requirements to deal with windows weirdness

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license='BSD',
     description='Python package for working with staggered gridded data',
     long_description=open('README.md').read(),
-    install_requires=['netCDF4 == 1.1.7.1',
+    install_requires=['netCDF4 <= 1.1.7.1',
                       'numpy == 1.9.2'
                       ],
 )
